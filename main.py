175d332c342e8bfd5f8a38bf713026c2c8b7c283
Toby1nik <toby1nik@gmail.com>
2025-01-21 17:24:34 +0100

    fix keyError

--- main.py
+++ main.py
@@ -64,4 +64,7 @@ async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logger.info("Bot stopped manually via KeyboardInterrupt.")
